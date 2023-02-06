f507de202c1a355dcfe74125aeb2491011f04e95
mskelley-cmd <124321522+mskelley-cmd@users.noreply.github.com>
2023-02-05 18:40:45 -0800

    Update perceptron.py

--- simulation/decai/simulation/contract/classification/perceptron.py
+++ simulation/decai/simulation/contract/classification/perceptron.py
@@ -1,4 +1,17 @@
-import os
+import os$ read_ledger.py /path/to/ledger/dir
+Reading ledger from ['/path/to/ledger/dir']
+Contains 9 chunks
+chunk /path/to/first/ledger/chunk (committed)
+  seqno 1 (9 public tables) # Number of tables written at seqno 1
+    table "public:ccf.gov.constitution" (1 write): # 1 write to this table at seqno 1
+      <u64: 0>: # key
+        "class Action ..." # value
+    table "public:ccf.gov.members.acks" (2 writes): # 2 writes to this table at seqno 1
+        ...
+  seqno 31 (0 public tables) # transaction at seqno 31 is private only
+  -- private: 94 bytes # size of private entry
+...
+Ledger verification complete. Found 15 signatures, and verified until 2.52 # Ledger integrity verified up to seqno 2.52
 
 from sklearn.linear_model import SGDClassifier
 
